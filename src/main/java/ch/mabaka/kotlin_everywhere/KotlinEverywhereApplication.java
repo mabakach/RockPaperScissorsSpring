baa8392fee5b4451b6d3f97526c2656e2be038a6
Marc Baumgartner <marc@mabaka.ch>
2019-09-10 14:35:46 +0200

    Empty project from spring.io

---
+++ src/main/java/ch/mabaka/kotlin_everywhere/KotlinEverywhereApplication.java
@@ -0,0 +1,13 @@
+package ch.mabaka.kotlin_everywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinEverywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinEverywhereApplication.class, args);
+	}
+
+}
